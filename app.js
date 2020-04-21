13d15f7e46065813d8525bdb453239c32bd9108c
Aaron Allehoff <51172401+aallehoff@users.noreply.github.com>
2020-04-21 16:53:38 -0400

    Implement authentication with database

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 'use strict'
 const express = require('express')
 const app = express()
+const sequelize = require('./models').sequelize
+
 const port = 3000
 
 app.get('/', (req, res) => {
     res.send('test response')
 })
 
-app.listen(port, () => {
-    console.log(`Server: listening on port ${port}`)
-})
+// Startup database and server.
+sequelize
+    // Test connection to database
+    .authenticate()
+    .then(() => {
+        console.log('App: authenticated connection to database.')
+        // Start Express server
+        app.listen(port, () => {
+            console.log(`App: server listening on port ${port}`)
+        })
+    })
+    .catch((err) => {
+        console.error('App: failed to authenticate connection to database.', err)
+    })
