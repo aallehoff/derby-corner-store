12195c224aa2888c93c921a55b263a324862a8d4
Aaron Allehoff <51172401+aallehoff@users.noreply.github.com>
2020-04-24 17:01:34 -0400

    Refactor and refine Item model

--- models/item.js
+++ models/item.js
@@ -1,14 +1,56 @@
-'use strict';
-module.exports = (sequelize, DataTypes) => {
-  const Item = sequelize.define('Item', {
-    upc: DataTypes.STRING,
-    name: DataTypes.STRING,
-    quantity: DataTypes.INTEGER,
-    priceInCents: DataTypes.INTEGER,
-    desc: DataTypes.STRING
-  }, {});
-  Item.associate = function(models) {
-    // associations can be defined here
-  };
-  return Item;
-};
+'use strict'
+const {Sequelize, Model} = require('sequelize')
+const {sequelize} = require('./index.js')
+
+class Item extends Model {}
+Item.init({
+  upc: {
+    type: Sequelize.STRING(12),
+    unique: true,
+    allowNull: false,
+    validate: {
+      isNumeric: true,
+      len: [12, 12]
+    }
+  },
+  productMfg: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  productName: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  quantityOnHand: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    validate: {
+      isInt: true,
+      min: 0
+    }
+  },
+  priceInCents: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    validate: {
+      isInt: true,
+      min: 0
+    }
+  }
+}, {sequelize})
+
+module.exports = Item
+
+// module.exports = (sequelize, DataTypes) => {
+//   const Item = sequelize.define('Item', {
+//     upc: DataTypes.STRING,
+//     name: DataTypes.STRING,
+//     quantity: DataTypes.INTEGER,
+//     priceInCents: DataTypes.INTEGER,
+//     desc: DataTypes.STRING
+//   }, {});
+//   Item.associate = function(models) {
+//     // associations can be defined here
+//   };
+//   return Item;
+// };
